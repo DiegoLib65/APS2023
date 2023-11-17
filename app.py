a5f308b80dc36ff7288e844269b9d7a29a85bc68
DiegoLib65 <47798333+DiegoLib65@users.noreply.github.com>
2023-11-16 23:16:48 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+# API - Lugar para disponibilizar recursos e/ou funcionalidades. 
+# 1. Objetivo - Criar um api qu disponibiliza a consulta, criação, edição e exclusão de livros.
+# 2. URL base - localhost
+# 3. Endpoints - 
+    # - localhost/livros (GET)
+    # - localhost/livros (POST)
+    # - localhost/livros/id (GET)
+    # - localhost/livro/id (PUT)
+    # - localhost/livro/id (DELETE)
+# 4. Quais recusrsos - Livros
+
+from flask import Flask, jsonify,request
+from db import livros
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+#Consultar(todos)
+@app.route('/livros',methods=['GET'])
+def obter_livros():
+    return jsonify(livros)
+
+#Consultar(id, nome ou Diretor)
+
+@app.route('/livros/<id>',methods=['GET'])
+def obter_livro_id(id):
+    livros_status = []
+    for livro in livros:
+        if livro.get('id') == id or livro.get('Diretor') == id or livro.get('titulo') == id or livro.get('estado') == id: 
+                livros_status.append(livro)
+    return jsonify(livros_status)
+
+#Editar um livro por ID
+@app.route('/livros/<id>',methods=['PUT'])
+def editar_livro_id(id):
+    livro_alterado = request.get_json()
+    for indice,livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
+#Criar novo livro
+@app.route('/livros', methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+
+    return jsonify(livros)
+
+#Excluir livro
+@app.route('/livros/<id>', methods=['DELETE'])
+def excluir_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+    
+    
+    return jsonify(livros)
+
+
+app.run(port=5000,host='0.0.0.0',debug=True)
---
+++ db.py
@@ -0,0 +1,213 @@
+livros = [
+    {
+        'id': '1',
+        'titulo': 'O Poderoso Chefão',
+        'Diretor': 'Francis Ford Coppola',
+        'estado': 'usado',
+        'quantidade_de_copias': 3
+
+    },
+    {
+        'id': '2',
+        'titulo': 'O Senhor dos Anéis: A Sociedade do Anel',
+        'Diretor': 'Peter Jackson',
+        'estado': 'usado',
+        'quantidade_de_copias': 10
+    },
+    {
+        'id': '3',
+        'titulo': 'Forrest Gump',
+        'Diretor': 'Robert Zemeckis',
+        'estado': 'novo',
+        'quantidade_de_copias': 5
+    },
+    {
+        'id': '4',
+        'titulo': 'O Labirinto do Fauno',
+        'Diretor': 'Guillermo del Toro',
+        'estado': 'usado',
+        'quantidade_de_copias': 10
+    },
+    {
+        'id': '5',
+        'titulo': 'Cidadão Kane',
+        'Diretor': 'Orson Welles ',
+        'estado': 'usado',
+        'quantidade_de_copias': 3
+    },
+        {
+        'id': '6',
+        'titulo': 'Interestelar',
+        'Diretor': 'Christopher Nolan',
+        'estado': 'novo',
+        'quantidade_de_copias': 4
+    },
+    {
+        'id': '7',
+        'titulo': 'A Lista de Schindler',
+        'Diretor': ' Steven Spielberg',
+        'estado': 'novo',
+        'quantidade_de_copias': 10
+    },
+    {
+        'id': '8',
+        'titulo': 'Pulp Fiction',
+        'Diretor': 'Quentin Tarantino',
+        'estado': 'usado',
+        'quantidade_de_copias': 14
+    },
+    {
+        'id': '9',
+        'titulo': 'Casablanca',
+        'Diretor': 'Michael Curtiz',
+        'estado': 'novo',
+        'quantidade_de_copias': 1
+    },
+    {
+        'id': '10',
+        'titulo': 'Cidade de Deus',
+        'Diretor': 'Fernando Meirelles e Kátia Lund',
+        'estado': 'novo',
+        'quantidade_de_copias': 2
+    },
+    {
+        'id': '11',
+        'titulo': 'Gladiador',
+        'Diretor': 'Ridley Scott',
+        'estado': 'usado',
+        'quantidade_de_copias': 1
+    },
+    {
+        'id': '12',
+        'titulo': 'O Silêncio dos Inocentes',
+        'Diretor': 'Jonathan Demme',
+        'estado': 'novo',
+        'quantidade_de_copias': 3
+    },
+    {
+        'id': '13',
+        'titulo': 'A Origem',
+        'Diretor': 'Christopher Nolan',
+        'estado': 'usado',
+        'quantidade_de_copias': 4
+    },
+    {
+        'id': '14',
+        'titulo': 'O Iluminado',
+        'Diretor': 'Stanley Kubrick',
+        'estado': 'novo',
+        'quantidade_de_copias': 7
+    },
+    {
+        'id': '15',
+        'titulo': 'Matrix',
+        'Diretor': 'The Wachowskis',
+        'estado': 'novo',
+        'quantidade_de_copias': 1
+    },
+    {
+        'id': '16',
+        'titulo': 'Psicose',
+        'Diretor': 'Alfred Hitchcock',
+        'estado': 'novo',
+        'quantidade_de_copias': 10
+    },
+    {
+        'id': '17',
+        'titulo': 'Laranja Mecânica',
+        'Diretor': 'Stanley Kubrick',
+        'estado': 'usado',
+        'quantidade_de_copias': 100
+    },
+    {
+        'id': '18',
+        'titulo': 'Clube da Luta',
+        'Diretor': 'David Fincher',
+        'estado': 'novo',
+        'quantidade_de_copias': 1
+    },
+    {
+        'id': '19',
+        'titulo': 'O Resgate do Soldado Ryan',
+        'Diretor': 'Steven Spielberg',
+        'estado': 'usado',
+        'quantidade_de_copias': 23
+    },
+    {
+        'id': '20',
+        'titulo': 'La La Land',
+        'Diretor': 'Damien Chazelle',
+        'estado': 'usado',
+        'quantidade_de_copias': 3
+    },
+    {
+        'id': '21',
+        'titulo': 'O Grande Lebowski',
+        'Diretor': 'Joel Coen',
+        'estado': 'usado',
+        'quantidade_de_copias': 5
+    },
+    {
+        'id': '22',
+        'titulo': 'O Pianista',
+        'Diretor': 'Roman Polanski',
+        'estado': 'usado',
+        'quantidade_de_copias': 5
+    },
+    {
+        'id': '23',
+        'titulo': 'A Vida é Bela',
+        'Diretor': 'Roberto Benigni',
+        'estado': 'usado',
+        'quantidade_de_copias': 4
+    },
+    {
+        'id': '24',
+        'titulo': 'A Forma da Água',
+        'Diretor': 'Guillermo del Toro',
+        'estado': 'usado',
+        'quantidade_de_copias': 5
+    },
+    {
+        'id': '25',
+        'titulo': 'E o Vento Levou ',
+        'Diretor': 'Victor Fleming',
+        'estado': 'novo',
+        'quantidade_de_copias': 5
+    },
+        {
+        'id': '26',
+        'titulo': 'Taxi Driver',
+        'Diretor': 'Martin Scorsese',
+        'estado': 'novo',
+        'quantidade_de_copias': 3
+    },
+    {
+        'id': '27',
+        'titulo': 'A Viagem de Chihiro',
+        'Diretor': 'Hayao Miyazaki',
+        'estado': 'novo',
+        'quantidade_de_copias': 10
+    },
+    {
+        'id': '28',
+        'titulo': 'A Vingança dos Sith',
+        'Diretor': 'Geroge Lucas',
+        'estado': 'usado',
+        'quantidade_de_copias': 5
+    },
+    {
+        'id': '29',
+        'titulo': 'A Origem dos Guardiões',
+        'Diretor': 'Peter Ramsey',
+        'estado': 'novo',
+        'quantidade_de_copias': 4
+    },
+    {
+        'id': '30',
+        'titulo': 'Clube dos Cincos',
+        'Diretor': 'John Hughes',
+        'estado': 'usado',
+        'quantidade_de_copias': 4
+    },
+]
